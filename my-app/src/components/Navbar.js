fb9bd01ad0d425300597bc4cb8168a4761b4d900
smcconaha <steve.mcconaha@student.awesomeinc.org>
2022-11-05 20:01:39 +0000

    Added nav bar functionality and styling

---
+++ my-app/src/components/Navbar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-dark">
+        <div className="container">
+            <a className="navbar-brand" href="#">Add Logo?</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <FontAwesomeIcon icon={faBars} style={{color: '#fff'}}/>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                    <a className="nav-link" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                     <a class="nav-link" href="#">About Me</a>
+                </li>
+                <li class="nav-item">
+                     <a class="nav-link" href="#">Portfolio</a>
+                </li>
+                <li class="nav-item">
+                     <a class="nav-link" href="#">Contact Me</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
