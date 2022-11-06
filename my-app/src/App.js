d1b8bcc4d667e551b8b4bfac8807a0d2faad40c7
smcconaha <steve.mcconaha@student.awesomeinc.org>
2022-11-06 22:38:32 +0000

    Added functionality to form submit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import Header from './components/Header';
 import AboutMe from './components/AboutMe';
 import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <AboutMe />
       <Portfolio />
+      <Contact />
     </>
   );
 }
